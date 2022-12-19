8729c8141ad846e84d57f509369fc8f53ec2053e
EmekaEmmanuel <emekaekeohaa@gmail.com>
2022-12-19 11:41:36 -0800

    Add changes for linter error

--- javascriptfile/menu.js
+++ javascriptfile/menu.js
@@ -1,18 +1,16 @@
-let hamburger = document.querySelector(".hamburger_img");
-let close_hamburger_btn = document.querySelector(".close_hamburger");
-let modal_menu_container = document.querySelector(".modal_menu_container");  
+const hamburger = document.querySelector('.hamburger_img');
+const closeHamburgerBtn = document.querySelector('.close_hamburger');
+const modalMenuContainer = document.querySelector('.modal_menu_container');
 
-
-
-const openMobileMenu = e => {
+const openMobileMenu = (e) => {
   e.preventDefault();
-  modal_menu_container.classList.remove('hide1') 
-}
+  modalMenuContainer.classList.remove('hide1');
+};
 
-const closeMobileMenu = e => {
-  e.preventDefault()
-  modal_menu_container.classList.add('hide1') 
-}
+const closeMobileMenu = (e) => {
+  e.preventDefault();
+  modalMenuContainer.classList.add('hide1');
+};
 
-hamburger.addEventListener("click", openMobileMenu);
-close_hamburger_btn.addEventListener("click", closeMobileMenu); 
+hamburger.addEventListener('click', openMobileMenu);
+closeHamburgerBtn.addEventListener('click', closeMobileMenu);
