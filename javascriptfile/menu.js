70d796315f251c7bb186adfbe1c88e04de4d5c1b
EmekaEmmanuel <emekaekeohaa@gmail.com>
2022-12-19 09:42:31 -0800

    Add change to allow routing with menu options

--- javascriptfile/menu.js
+++ javascriptfile/menu.js
@@ -1,4 +1,4 @@
-let hamburger_btn = document.querySelector(".hamburger_img");
+let hamburger = document.querySelector(".hamburger_img");
 let close_hamburger_btn = document.querySelector(".close_hamburger");
 let modal_menu_container = document.querySelector(".modal_menu_container");  
 
@@ -14,5 +14,5 @@ const closeMobileMenu = e => {
   modal_menu_container.classList.add('hide1') 
 }
 
-hamburger_btn.addEventListener("click", openMobileMenu);
+hamburger.addEventListener("click", openMobileMenu);
 close_hamburger_btn.addEventListener("click", closeMobileMenu); 
