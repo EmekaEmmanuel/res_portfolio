c3c7a8679fc213ee22894eb5662b69b69af93683
BB-Simon <simon.chowdery@gmail.com>
2022-12-19 17:18:18 +0200

    Add open and close menu function

--- javascriptfile/menu.js
+++ javascriptfile/menu.js
@@ -1,3 +1,15 @@
 let hamburger_btn = document.querySelector(".hamburger_img")
 let close_hamburger_btn = document.querySelector(".close_hamburger")
 let modal_menu_container = document.querySelector(".hamburger_img")
+
+hamburger_btn.addEventListener("click", openMobileMenu);
+close_hamburger_btn.addEventListener("click", closeMobileMenu);
+
+const openMobileMenu = e => {
+  e.preventDefault();
+  modal_menu_container.classList.add('block1')
+}
+const closeMobileMenu = e => {
+  e.preventDefault()
+  modal_menu_container.classList.remove('block1')
+}
