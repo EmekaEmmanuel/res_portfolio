5aa91b1dcd2e89dee45435e28c41db0e1c5d04e6
BB-Simon <simon.chowdery@gmail.com>
2022-12-21 16:29:24 +0200

    Add validate input on change function

--- javascriptfile/validate.js
+++ javascriptfile/validate.js
@@ -2,4 +2,23 @@ const inputName = document.querySelector('#name').value;
 const inputEmail = document.querySelector('#email').value;
 const inputMessage = document.querySelector('#text_message').value;
 
+const values = {
+  inputName,
+  inputEmail,
+  inputMessage,
+}
 
+const validateInputsOnChange = (value) => {
+  const errors = {};
+  if(value.inputName === ""){
+    errors.name = "Name is required"
+  }
+  if(value.inputEmail === ""){
+    errors.email = "Email is required"
+  }
+  if(value.inputMessage === ""){
+    errors.message = "Message is required"
+  }
+}
+
+validateInputsOnChange(values)
