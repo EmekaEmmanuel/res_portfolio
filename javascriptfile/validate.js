a14c9b431547faa623b56f8565eab8375b7f0fe0
BB-Simon <simon.chowdery@gmail.com>
2022-12-21 16:24:59 +0200

    Add validate js file

---
+++ javascriptfile/validate.js
@@ -0,0 +1,5 @@
+const inputName = document.querySelector('#name').value;
+const inputEmail = document.querySelector('#email').value;
+const inputMessage = document.querySelector('#text_message').value;
+
+
