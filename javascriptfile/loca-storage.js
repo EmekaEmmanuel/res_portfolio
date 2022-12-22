655354ccf7417b6e34e5f541f391719abe8c5f76
BB-Simon <simon.chowdery@gmail.com>
2022-12-22 16:17:48 +0200

    Save form data in local storage method

--- javascriptfile/loca-storage.js
+++ javascriptfile/loca-storage.js
@@ -6,4 +6,20 @@ contactForm.addEventListener('submit', onSubmit);
 const onSubmit = e => {
   e.preventDefault();
   console.log(e);
+  // saveFormDataToLocalStorage(inputValues)
+}
+
+const saveFormDataToLocalStorage = (inputValues) => {
+  const formdata = {};
+  if(inputValues.name) {
+    formdata.name = inputValues.name;
+  }
+  if(inputValues.email) {
+    formdata.name = inputValues.email;
+  }
+  if(inputValues.message) {
+    formdata.name = inputValues.message;
+  }
+
+  window.localStorage.setItem('formdata', formdata);
 }
