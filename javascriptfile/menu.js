db3409d4260dac488ed281f17ded59d3b94d054c
EmekaEmmanuel <emekaekeohaa@gmail.com>
2022-12-21 22:50:35 -0800

    Add changes for linter errors

--- javascriptfile/menu.js
+++ javascriptfile/menu.js
@@ -2,58 +2,57 @@ const hamburger = document.querySelector('.hamburger_img');
 const closeHamburgerBtn = document.querySelector('.close_hamburger');
 const modalMenuContainer = document.querySelector('.modal_menu_container');
 const menuOption = document.querySelectorAll('.menu_option');
-const seeProjectDetailsBtn = document.querySelector('#see-projects-btn');
 const worksSection = document.querySelector('.works_section');
 
 const cardsData = [
   {
     id: 0,
-    name: ['Tonic', "Tonic"],
+    name: ['Tonic', 'Tonic'],
     description: [
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.", "A daily selection of privately personalized reads; no accounts or sign-ups required."],
-    image: ["../img/jumbo1.jpg", "../img/jumbo5.png"],
-    technologies: ["html", " css ", "javaScript"],
-    technologiesDesk: ["html", "Ruby on rails", "css", "javaScript"],
-    technologiesPop: ["html", " css", "javaScript", "github", "ruby", "Bootstraps"],
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.', 'A daily selection of privately personalized reads; no accounts or sign-ups required.'],
+    image: ['../img/jumbo1.jpg', '../img/jumbo5.png'],
+    technologies: ['html', ' css ', 'javaScript'],
+    technologiesDesk: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    technologiesPop: ['html', ' css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     Source: 'https://github.com/emekaemmanuel/res_portfolio',
     liveLink: 'https://github.com/emekaemmanuel/res_portfolio',
   },
 
   {
     id: 1,
-    name: ["Multi-Post Stories", "Multi-Post Stories"],
+    name: ['Multi-Post Stories', 'Multi-Post Stories'],
     description: [
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.", "A daily selection of privately personalized reads; no accounts or sign-ups required.", "Experimental content creation feature that allows users to add to an existing story over the course a day without spamming their friends."],
-    image: ["../img/jumbo4.jpg", "../img/jumbo4b.jpg"],
-    technologies: ["html", " css ", "javaScript"],
-    technologiesDesk: ["html", "Ruby on rails", "css", "javaScript"],
-    technologiesPop: ["html", " css", "javaScript", "github", "ruby", "Bootstraps"],
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.', 'A daily selection of privately personalized reads; no accounts or sign-ups required.', 'Experimental content creation feature that allows users to add to an existing story over the course a day without spamming their friends.'],
+    image: ['../img/jumbo4.jpg', '../img/jumbo4b.jpg'],
+    technologies: ['html', ' css ', 'javaScript'],
+    technologiesDesk: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    technologiesPop: ['html', ' css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     Source: 'https://github.com/emekaemmanuel/res_portfolio',
     liveLink: 'https://github.com/emekaemmanuel/res_portfolio',
   },
 
   {
     id: 2,
-    name: ["Tonic", "Facebook 360"],
+    name: ['Tonic', 'Facebook 360'],
     description:
-      ["A daily selection of privately personalized reads; no accounts or sign-ups required.", "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR..."],
-    image: ["../img/jumbo3.jpg", "../img/jumbo1b.png"],
-    technologies: ["html", " css ", "javaScript"],
-    technologiesDesk: ["html", "Ruby on rails", "css", "javaScript"],
-    technologiesPop: ["html", " css", "javaScript", "github", "ruby", "Bootstraps"],
+      ['A daily selection of privately personalized reads; no accounts or sign-ups required.', "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR..."],
+    image: ['../img/jumbo3.jpg', '../img/jumbo1b.png'],
+    technologies: ['html', ' css ', 'javaScript'],
+    technologiesDesk: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    technologiesPop: ['html', ' css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     Source: 'https://github.com/emekaemmanuel/res_portfolio',
     liveLink: 'https://github.com/emekaemmanuel/res_portfolio',
   },
 
   {
     id: 3,
-    name: ["Multi-Post Stories", "Uber Navigation"],
+    name: ['Multi-Post Stories', 'Uber Navigation'],
     description:
-      ["A daily selection of privately personalized reads; no accounts or sign-ups required.", "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car."],
-    image: ["../img/jumbo4.jpg", "../img/jumbo2b.png"],
-    technologies: ["html", " css ", "javaScript"],
-    technologiesDesk: ["html", "Ruby on rails", "css", "javaScript"],
-    technologiesPop: ["html", " css", "javaScript", "github", "ruby", "Bootstraps"],
+      ['A daily selection of privately personalized reads; no accounts or sign-ups required.', 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.'],
+    image: ['../img/jumbo4.jpg', '../img/jumbo2b.png'],
+    technologies: ['html', ' css ', 'javaScript'],
+    technologiesDesk: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    technologiesPop: ['html', ' css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
     Source: 'https://github.com/emekaemmanuel/res_portfolio',
     liveLink: 'https://github.com/emekaemmanuel/res_portfolio',
   },
@@ -73,10 +72,8 @@ menuOption.forEach((e) => {
   };
 });
 
-let displayCard = ""
-for (let i = 0; i < cardsData.length; i++) {
-  let even = i % 2 === 0;
-  console.log(even, i);
+let displayCard = '';
+for (let i = 0; i < cardsData.length; i += 1) {
   displayCard += `
   
   <article class="works_article bg_color7">
@@ -155,73 +152,21 @@ for (let i = 0; i < cardsData.length; i++) {
 
         </article>
 
-  `
+  `;
 }
 
-worksSection.innerHTML = ""
-worksSection.innerHTML = displayCard
+worksSection.innerHTML = '';
+worksSection.innerHTML = displayCard;
 
-console.log(worksSection);
-console.log(cardsData);
-
-
-
-
-
-// const showModal = () => {
-//   const btn = document.querySelectorAll('.work-btn');
-//   const modalContainer = document.createElement('section');
-//   modalContainer.className = 'modal-container';
-//   const modalMobile = document.createElement('section');
-//   modalMobile.className = 'modal-container-mobile';
-//   btn.forEach((e) => {
-//     e.addEventListener('click', () => {k
-//       const index = e.getAttribute('ownIndex');
-//       const forLaptop = modalElem(projectArr[index]);
-//       const forMobile = modalMob(projectArr[index]);
-//       const body = document.querySelector('body');
-//       body.append(modalContainer);
-//       body.append(modalMobile);
-//       const modalContain = document.querySelector('.modal-container');
-//       const mobileContain = document.querySelector('.modal-container-mobile');
-//       mobileContain.innerHTML = forMobile;
-//       modalContain.innerHTML = forLaptop;
-//       const closeModal = document.querySelectorAll('.close-modal');
-//       closeModal.forEach((e) => {
-//         e.addEventListener('click', () => {
-//           modalMobile.remove();
-//           modalContain.remove();
-//         });
-//       });
-//     });
-//   });
-// };
-// showModal();
-
-
-
-
-
-// console.log(popupContent(cardsData));
-
-// const displayPopup = () => {
 const showPopupBtn = document.querySelectorAll('.seepopup');
-// console.log(showPopupBtn);
 const popUpContainer = document.createElement('section');
-popUpContainer.className = "popup_section"
-popUpContainer.classList.add("bg_color6")
-// console.log(popUpContainer);
-showPopupBtn.forEach(e => {
-  console.log(e.type);
+popUpContainer.className = 'popup_section';
+popUpContainer.classList.add('bg_color6');
+showPopupBtn.forEach((e) => {
   e.addEventListener('click', () => {
-    console.log(e);
-    const btnIndex = e.getAttribute("index")
-    console.log(btnIndex);
+    const btnIndex = e.getAttribute('index');
 
-    // 
-    let popupEle =
-
-      `
+    const popupEle = `
   <article class="popup_article bg_color7">
 
 <div class="popup_articlediv show1">
@@ -344,27 +289,18 @@ showPopupBtn.forEach(e => {
 </article>
 `;
 
-console.log(popupEle);
-
-    // popupContent(cardsData[btnIndex]) 
-
     const body = document.querySelector('body');
-    console.log(body);
-    console.log(popUpContainer);
-    // console.log(body.insertAdjacentHTML("afterbegin", popUpContainer)); 
-   console.log( body.appendChild(popUpContainer));
-
+    body.appendChild(popUpContainer);
     const popUpBox = document.querySelector('.popup_section');
     popUpBox.innerHTML = popupEle;
-    console.log(popUpBox);
     const closePopup = document.querySelectorAll('.close_popup');
     closePopup.forEach((e) => {
       e.addEventListener('click', () => {
         popUpContainer.remove();
       });
     });
-  })
-})
+  });
+});
 // }
 
 // displayPopup()
